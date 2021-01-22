020f5e8ed2abea8a22022c783f4be261dced4966
Alex Arze <alexarze@gmail.com>
2021-01-22 01:29:26 -0600

    Added semicolons to follow style guidelines

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,11 +17,11 @@
  * Define Global Variables
  * 
 */
-let sections = []
-let breakpoints = []
-let navNode
-let navHeight
-let curActive = -1
+let sections = [];
+let breakpoints = [];
+let navNode;
+let navHeight;
+let curActive = -1;
 
 /**
  * End Global Variables
@@ -30,65 +30,65 @@ let curActive = -1
 */
 
 function findSections() {
-    sectionList = document.querySelectorAll('section')
+    sectionList = document.querySelectorAll('section');
     sectionList.forEach((section) => {
         sections.push({
             element: section,
             nav: section.getAttribute('data-nav'),
             id: section.getAttribute('id')
-        })
+        });
 
-        breakpoints.push(section.getBoundingClientRect().top)
-    })
-    console.log(sections)
+        breakpoints.push(section.getBoundingClientRect().top);
+    });
+    console.log(sections);
 }
 
 function recalc() {
     // navigation height
-    navHeight = navNode.getBoundingClientRect().height
+    navHeight = navNode.getBoundingClientRect().height;
 
     // section heights
     for (let i = 0; i < sections.length; i++) {
-        breakpoints[i] = sections[i].element.getBoundingClientRect().top + window.pageYOffset
+        breakpoints[i] = sections[i].element.getBoundingClientRect().top + window.pageYOffset;
     }
 
     // update the active section
-    updateActive()
+    updateActive();
 }
 
 function currentPosition() {
-    return window.pageYOffset + Math.max(window.innerHeight / 2, navHeight)
+    return window.pageYOffset + Math.max(window.innerHeight / 2, navHeight);
 }
 
 function updateCurrentActive() {
     // Updates the current active section after a non-user scroll
-    const currPosition = currentPosition()
-    let newActive = -1
+    const currPosition = currentPosition();
+    let newActive = -1;
 
     for (let i = 0; i < breakpoints.length; i++) {
         if (breakpoints[i] < currPosition) {
-            newActive = i
+            newActive = i;
         } else {
-            break
+            break;
         }
     }
 
-    unsetActive(curActive)
-    setActive(newActive)
-    curActive = newActive
+    unsetActive(curActive);
+    setActive(newActive);
+    curActive = newActive;
 }
 
 function setActive(i) {
     if (i > -1 && i < sections.length) {
-        sections[i].element.classList.add('your-active-class')
-        sections[i].navElement.classList.add('active')
+        sections[i].element.classList.add('your-active-class');
+        sections[i].navElement.classList.add('active');
     }
 }
 
 function unsetActive(i) {
     if (i > -1 && i < sections.length) {
-        sections[i].element.classList.remove('your-active-class')
-        sections[i].navElement.classList.remove('active')
+        sections[i].element.classList.remove('your-active-class');
+        sections[i].navElement.classList.remove('active');
     }
 }
 
@@ -100,82 +100,82 @@ function unsetActive(i) {
 
 // main function
 function main() {
-    updateCurrentActive()
-    updateActive()
+    updateCurrentActive();
+    updateActive();
 }
 
 // build the nav
 function buildNav() {
     // init
-    navNode = document.getElementById('navbar__list')
+    navNode = document.getElementById('navbar__list');
 
     // find the sections for navigation
-    findSections()
+    findSections();
 
     // update the nav list
-    navNode.hidden = true
+    navNode.hidden = true;
     sections.forEach((section) => {
-        const newItem = document.createElement('li')
+        const newItem = document.createElement('li');
 
         // set up newItem
-        newItem.classList.add('menu__link')
-        newItem.textContent = section.nav
-        newItem.setAttribute('data-nav', section.id)
-        newItem.addEventListener('click', scrollToAnchor)
+        newItem.classList.add('menu__link');
+        newItem.textContent = section.nav;
+        newItem.setAttribute('data-nav', section.id);
+        newItem.addEventListener('click', scrollToAnchor);
 
         // add newItem to dictionary for reference
-        section['navElement'] = newItem
+        section['navElement'] = newItem;
 
         // append to menu
-        navNode.appendChild(newItem)
-    })
-    navNode.hidden = false
+        navNode.appendChild(newItem);
+    });
+    navNode.hidden = false;
 
-    navHeight = navNode.getBoundingClientRect().height
+    navHeight = navNode.getBoundingClientRect().height;
 }
 
 
 // Add class 'active' to section when near top of viewport
 function updateActive() {
-    const curPosition = currentPosition()
-    let newActive = curActive
+    const curPosition = currentPosition();
+    let newActive = curActive;
 
     const top = (curActive == -1) 
         ? 0 
-        : breakpoints[curActive]
+        : breakpoints[curActive];
     
     const bottom = (curActive == breakpoints.length - 1) 
         ? document.body.clientHeight 
-        : breakpoints[curActive + 1]
+        : breakpoints[curActive + 1];
 
     if (curPosition < top) {
-        newActive = curActive - 1
+        newActive = curActive - 1;
     } else if (curPosition > bottom) {
-        newActive = curActive + 1
+        newActive = curActive + 1;
     }
 
     if (newActive != curActive) {
-        unsetActive(curActive)
-        setActive(newActive)
-        curActive = newActive
+        unsetActive(curActive);
+        setActive(newActive);
+        curActive = newActive;
     }
 }
 
 
 // Scroll to anchor ID using scrollTO event
 function scrollToAnchor(event) {
-    const element = document.getElementById(event.target.getAttribute('data-nav'))
-    const top = element.getBoundingClientRect().top
-    const height = navHeight
-    const yOffset = window.pageYOffset
+    const element = document.getElementById(event.target.getAttribute('data-nav'));
+    const top = element.getBoundingClientRect().top;
+    const height = navHeight;
+    const yOffset = window.pageYOffset;
 
     window.scrollTo({
         top: top - height + yOffset,
         behavior: 'smooth'
-    })
+    });
 
-    updateCurrentActive()
-    updateActive()
+    updateCurrentActive();
+    updateActive();
 }
 
 
@@ -186,13 +186,13 @@ function scrollToAnchor(event) {
 */
 
 // Build menu 
-document.addEventListener('DOMContentLoaded', buildNav)
-window.addEventListener('load', main)
+document.addEventListener('DOMContentLoaded', buildNav);
+window.addEventListener('load', main);
 
 // Set sections as active
-window.addEventListener('scroll', updateActive, {passive: true})
+window.addEventListener('scroll', updateActive, {passive: true});
 
-window.addEventListener('resize', recalc)
+window.addEventListener('resize', recalc);
 
 /**
  * End Events
@@ -200,4 +200,4 @@ window.addEventListener('resize', recalc)
  */
 
 //  Prevent automatic scroll recovery
-history.scrollRestoration = 'manual'
+history.scrollRestoration = 'manual';
